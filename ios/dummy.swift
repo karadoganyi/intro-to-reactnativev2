90e40f3ff4e5ef2ce0823b3cbb77a6e47ed98e90
karadogan <yusufkaradogan@karadogans-MacBook-Air.local>
2023-02-05 11:54:02 +0300

    UI Elements

---
+++ ios/dummy.swift
@@ -0,0 +1,8 @@
+//
+//  dummy.swift
+//  MoodTracker
+//
+//  Created by karadogan on 3.02.2023.
+//
+
+import Foundation
---
+++ src/theme.ts
@@ -0,0 +1,1 @@
+export const theme = { colorPurple: '#454C73', colorWhite: '#fff' };
